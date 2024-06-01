8fb125624e9da0e869eb64ff6f14754512bdae71
Brandon Nguyen <brandon@nguyen.computer>
2024-06-01 02:55:35 -0400

    nvim: Add rust-analyzer lsp config

--- packages/nvim/.config/nvim/plugin/lsp.lua
+++ packages/nvim/.config/nvim/plugin/lsp.lua
@@ -1,6 +1,7 @@
 -- My personal LSP configurations
 -- Takes advantage of nvim-lspconfig
 require'lspconfig'.clangd.setup{}
+require'lspconfig'.rust_analyzer.setup{}
 
 -- Fix code issues via code action
 vim.keymap.set('n', '<C-Space>', vim.lsp.buf.code_action);
