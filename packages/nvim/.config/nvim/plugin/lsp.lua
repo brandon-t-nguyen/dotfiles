464ad8addbb0665b4e4253902cc9922b11430196
Brandon Nguyen <brandon@nguyen.computer>
2024-07-07 14:29:59 -0400

    nvim: Add pylsp for Python LSP

--- packages/nvim/.config/nvim/plugin/lsp.lua
+++ packages/nvim/.config/nvim/plugin/lsp.lua
@@ -2,6 +2,21 @@
 -- Takes advantage of nvim-lspconfig
 require'lspconfig'.clangd.setup{}
 require'lspconfig'.rust_analyzer.setup{}
+require'lspconfig'.pylsp.setup{
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = {'W391'},
+          maxLineLength = 100
+        },
+        pylint = {
+            enabled = true
+        }
+      }
+    }
+  }
+}
 
 -- Fix code issues via code action
 vim.keymap.set('n', '<C-Space>', vim.lsp.buf.code_action);
